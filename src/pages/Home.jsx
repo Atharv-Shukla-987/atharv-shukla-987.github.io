c5c3d3afe1a02ae3dff5b44d6d6c4dd520747906
Atharv-Shukla-987 <249597135+Atharv-Shukla-987@users.noreply.github.com>
2026-04-08 23:10:19 +0530

    Update video source to external URL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className=''>
       <Navbar/>
       <div className='h-screen w-screen fixed'>
-        <video autoPlay muted loop src='public\video\69496b2d.mp4'/>
+        <video autoPlay muted loop src='https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1775669859~exp=1775673459~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=9d8ae10319fd9c2eed95e6f55061d5bf379b5431e782b027c0890db785d910fa&r=dXMtZWFzdDE%3D'/>
       </div>
      <div className='h-screen w-screen relative flex flex-col justify-between overflow-hidden
       '>
